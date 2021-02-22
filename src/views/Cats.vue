ee5b04d226c842564eaff91473beccacc4607678
kk0055 <rtsw.lgy@gmail.com>
2021-02-22 22:42:33 +0800

    Cats リファクタ

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <h1>Cats for adoption</h1>
-    <b-table striped hover :items="cats">
-    <template slot="name" slot-scope="data">
-     <router-link :to="`/pets/${data.index}`">
-     {{ data.value }} 
-     </router-link>
-   </template>
-
-    </b-table>
+  <petTable
+  species="cats"
+  :pets="cats"
+  />
   </div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import PetTable from '@/components/PetTable.vue'
 export default {
+  components: {
+    PetTable
+  },
   data() {
     return {
 
